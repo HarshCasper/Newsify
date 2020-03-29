4938a44d61584f3b0d818fa4d431245f44ed4908
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-03-29 20:20:07 +0530

    Add files via upload

---
+++ __tests__/App.js
@@ -0,0 +1,10 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../src/App';
+
+// Note: test renderer must be required after react-native.
+
+it('renders correctly', () => {
+  const tree = renderer.create(<App />);
+});
